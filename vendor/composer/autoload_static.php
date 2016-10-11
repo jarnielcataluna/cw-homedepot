f21dff73613fc64f7da065e1c98a63cfe86e7772
jarnielcataluna <jarnielcataluna@searchoptmedia.com>
2016-10-11 13:12:43 +0800

    Inital Commit for Staging

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -7,88 +7,17 @@ namespace Composer\Autoload;
 class ComposerStaticInit269dd6d9f0a6769e176a6d99a5ab740f
 {
     public static $prefixLengthsPsr4 = array (
-        'S' => 
-        array (
-            'Symfony\\Component\\Routing\\' => 26,
-            'Symfony\\Component\\HttpKernel\\' => 29,
-            'Symfony\\Component\\HttpFoundation\\' => 33,
-            'Symfony\\Component\\EventDispatcher\\' => 34,
-            'Symfony\\Component\\Debug\\' => 24,
-            'Symfony\\Bridge\\Twig\\' => 20,
-            'Silex\\' => 6,
-        ),
         'P' => 
         array (
             'Picqer\\Barcode\\' => 15,
         ),
-        'M' => 
-        array (
-            'Monolog\\' => 8,
-        ),
     );
 
     public static $prefixDirsPsr4 = array (
-        'Symfony\\Component\\Routing\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/symfony/routing',
-        ),
-        'Symfony\\Component\\HttpKernel\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/symfony/http-kernel',
-        ),
-        'Symfony\\Component\\HttpFoundation\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/symfony/http-foundation',
-        ),
-        'Symfony\\Component\\EventDispatcher\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/symfony/event-dispatcher',
-        ),
-        'Symfony\\Component\\Debug\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/symfony/debug',
-        ),
-        'Symfony\\Bridge\\Twig\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/symfony/twig-bridge',
-        ),
-        'Silex\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/silex/silex/src/Silex',
-        ),
         'Picqer\\Barcode\\' => 
         array (
             0 => __DIR__ . '/..' . '/picqer/php-barcode-generator/src',
         ),
-        'Monolog\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/monolog/monolog/src/Monolog',
-        ),
-    );
-
-    public static $prefixesPsr0 = array (
-        'T' => 
-        array (
-            'Twig_' => 
-            array (
-                0 => __DIR__ . '/..' . '/twig/twig/lib',
-            ),
-        ),
-        'P' => 
-        array (
-            'Psr\\Log\\' => 
-            array (
-                0 => __DIR__ . '/..' . '/psr/log',
-            ),
-            'Pimple' => 
-            array (
-                0 => __DIR__ . '/..' . '/pimple/pimple/lib',
-            ),
-        ),
-    );
-
-    public static $classMap = array (
-        'SessionHandlerInterface' => __DIR__ . '/..' . '/symfony/http-foundation/Resources/stubs/SessionHandlerInterface.php',
     );
 
     public static function getInitializer(ClassLoader $loader)
@@ -96,8 +25,6 @@ class ComposerStaticInit269dd6d9f0a6769e176a6d99a5ab740f
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInit269dd6d9f0a6769e176a6d99a5ab740f::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInit269dd6d9f0a6769e176a6d99a5ab740f::$prefixDirsPsr4;
-            $loader->prefixesPsr0 = ComposerStaticInit269dd6d9f0a6769e176a6d99a5ab740f::$prefixesPsr0;
-            $loader->classMap = ComposerStaticInit269dd6d9f0a6769e176a6d99a5ab740f::$classMap;
 
         }, null, ClassLoader::class);
     }
