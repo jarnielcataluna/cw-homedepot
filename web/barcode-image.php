654eacfd69097c4759c9bfa1ce9c8161f614183d
jarnielcataluna <jarnielcataluna@searchoptmedia.com>
2016-10-11 13:13:55 +0800

    Inital Commit for Staging

--- web/barcode-image.php
+++ web/barcode-image.php
@@ -1,15 +1,15 @@
 <?php
-ini_set('display_errors', '1');
+//ini_set('display_errors', '1');
 require_once(dirname(__FILE__).'/../vendor/autoload.php');
-//header('Content-Type: image/png');
+header('Content-Type: image/png');
 
 $generator = new \Picqer\Barcode\BarcodeGeneratorPNG();
 
 $data = $generator->getBarcode($_GET['code'], $generator::TYPE_EAN_13, 2, 100, array(0,0,0));
 $im = imagecreatefromstring($data);
 
-//imagepng($im);
-//imagedestroy($im);
+imagepng($im);
+imagedestroy($im);
 
 
 ?>
